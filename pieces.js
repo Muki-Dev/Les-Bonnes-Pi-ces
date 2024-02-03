b961a57baab90901fce81b0086819cec486edaba
Muki Dev <mukidev881@gmail.com>
2024-02-03 03:15:08 +0100

    creation des elements

--- pieces.js
+++ pieces.js
@@ -1,6 +1,15 @@
 // Rècuperation des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
-console.log(pieces);
 
 // Créez de nouveaux élément dans le DOM
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} €`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+console.log(categorieElement.innerText);
