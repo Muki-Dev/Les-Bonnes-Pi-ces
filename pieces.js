52f800912db350d69182724084bc74a0ff49a5df
Muki Dev <mukidev881@gmail.com>
2024-02-03 03:48:44 +0100

    generer description et disponibilite dans le dom

--- pieces.js
+++ pieces.js
@@ -12,16 +12,25 @@ let prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} €`;
 let categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie;
+let descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description;
+let disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilite;
+
+
 
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
+
 
 // Vérification de données
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€":"€€€"})`
-
+disponibiliteElement.innerText = article.disponibilite ? "En Stock" : "Rupture de stock";
 categorieElement.innerText = article.categorie ?? "(aucune categorie)";
-
-document.body.appendChild(prixElement,categorieElement);
+descriptionElement.innerText = article.description ?? ("Pas de description pour le moment.")
+document.body.appendChild(prixElement,categorieElement,descriptionElement,disponibiliteElement);
