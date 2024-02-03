7fff8f9e3a0adcb71305aad635d385c2bf181ef3
Muki Dev <mukidev881@gmail.com>
2024-02-03 03:00:06 +0100

    importer npm et fix fetch bug

--- pieces.js
+++ pieces.js
@@ -1,3 +1,6 @@
 // Rècuperation des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-auto.json");
+const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
+console.log(pieces);
+
+// Créez de nouveaux élément dans le DOM
