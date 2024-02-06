2a2d03dfd7d31d83e6626cd8d95250b744f97345
Muki Dev <mukidev881@gmail.com>
2024-02-06 12:58:09 +0100

    afficher la liste avec un boucle for

--- pieces.js
+++ pieces.js
@@ -2,20 +2,25 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
+for(let i = 0; i < pieces.length; i++){
 // Créez de nouveaux élément dans le DOM
-const article = pieces[0];
 const imageElement = document.createElement("img");
-imageElement.src = article.image;
+imageElement.src = pieces[i].image;
+
 const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
+nomElement.innerText = pieces[i].nom;
+
 let prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} €`;
+prixElement.innerText = `Prix: ${pieces[i].prix} €`;
+
 let categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
+categorieElement.innerText = pieces[i].categorie;
+
 let descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description;
+descriptionElement.innerText = pieces[i].description;
+
 let disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite;
+disponibiliteElement.innerText = pieces[i].disponibilite;
 
 
 
@@ -27,10 +32,16 @@ sectionFiches.appendChild(categorieElement);
 sectionFiches.appendChild(descriptionElement);
 sectionFiches.appendChild(disponibiliteElement);
 
-
-// Vérification de données
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€":"€€€"})`
-disponibiliteElement.innerText = article.disponibilite ? "En Stock" : "Rupture de stock";
-categorieElement.innerText = article.categorie ?? "(aucune categorie)";
-descriptionElement.innerText = article.description ?? ("Pas de description pour le moment.")
+//Vérification de données
+prixElement.innerText = `Prix: ${pieces[i].prix} € (${pieces[i].prix < 35 ? "€":"€€€"})`
+disponibiliteElement.innerText = pieces[i].disponibilite ? "En Stock" : "Rupture de stock";
+categorieElement.innerText = pieces[i].categorie ?? "(aucune categorie)";
+descriptionElement.innerText = pieces[i].description ?? ("Pas de description pour le moment.")
 document.body.appendChild(prixElement,categorieElement,descriptionElement,disponibiliteElement);
+
+
+
+
+}
+
+
