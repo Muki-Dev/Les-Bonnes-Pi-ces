df6020d51b333b4efa559a474e410f8efe369c4c
Muki Dev <mukidev881@gmail.com>
2024-02-11 02:00:55 +0100

    Recupérer identifiant de la pièce automobile

--- avis.js
+++ avis.js
@@ -3,7 +3,8 @@ export function ajoutListenersAvis() {
 
     for (let i = 0; i < piecesElements.length; i++) {
       piecesElements[i].addEventListener("click", async function (event) {
-           /* ... */
+        const id = event.target.dataset.id;
+        fetch(`http://localhost:8081/pieces/${id}/avis`);
       });
     }
 }
